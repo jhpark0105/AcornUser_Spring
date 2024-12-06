5fcfb4156fe22b78d78de2d439973cb66f954cbd
Kai Jang <mmzz6699@gmail.com>
2024-12-06 15:12:22 +0900

    공지 상세페이지 버튼기능 추가
    이전/다음버튼에 해당하는 공지번호를 가져오는 로직 추가, 엔티티 클래스에 컬럼명 추가

--- src/main/java/com/erp/entity/Notice.java
+++ src/main/java/com/erp/entity/Notice.java
@@ -23,18 +23,19 @@ import lombok.NoArgsConstructor;
 public class Notice {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+	@Column(name = "notice_no")
 	private int noticeNo; // 공지 번호
 	
-	@Column(length = 50, nullable = false)
+	@Column(name = "notice_title", length = 50, nullable = false)
 	private String noticeTitle; // 공지 제목
 	
-	@Column(length = 1000, nullable = false)
+	@Column(name = "notice_content", length = 1000, nullable = false)
 	private String noticeContent; // 공지 내용
 	
-	@Column(nullable = false, updatable = false)
+	@Column(name = "notice_reg", nullable = false, updatable = false)
 	private LocalDate noticeReg; // 공지 작성일
 	
-	@Column(nullable = false, columnDefinition = "tinyint(1) default 0")
+	@Column(name = "notice_check", nullable = false, columnDefinition = "tinyint(1) default 0")
 	private boolean noticeCheck; // 공지 중요여부
 	
 	@PrePersist // persist되기 전에 자동호출되어 당일 날짜정보 저장
--- src/main/java/com/erp/repository/NoticeRepository.java
+++ src/main/java/com/erp/repository/NoticeRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.erp.dto.NoticeNoOnly;
 import com.erp.entity.Notice;
 
 public interface NoticeRepository extends JpaRepository<Notice, Integer>{
@@ -20,5 +21,8 @@ public interface NoticeRepository extends JpaRepository<Notice, Integer>{
 	/* Dashboard 공지사항 목록 출력용 */
 	@Query("SELECT n FROM Notice n WHERE n.noticeCheck=true ORDER BY n.noticeReg desc")
 	List<Notice> getCheckedNoticeList();
-	
+
+	// 이전/다음버튼 클릭 시 해당하는 공지번호 쿼리용
+	NoticeNoOnly findFirstByNoticeNoLessThanOrderByNoticeNoDesc(int currentNo);
+	NoticeNoOnly findFirstByNoticeNoGreaterThanOrderByNoticeNoAsc(int currentNo);
 }
