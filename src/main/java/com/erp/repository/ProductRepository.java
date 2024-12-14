a2aa22d6cbc2117d2678bae64a3198cbf9443878
woojin0119 <noah0119@naver.com>
2024-12-14 14:38:41 +0900

    Product 화면 대분류에 소분류 존재할 때, 삭제 불가능

--- src/main/java/com/erp/repository/ProductRepository.java
+++ src/main/java/com/erp/repository/ProductRepository.java
@@ -11,6 +11,10 @@ public interface ProductRepository extends JpaRepository<Product, String> {
 	//소분류 코드로 소분류 데이터 조회
 	@Query("SELECT p FROM Product p WHERE p.productCode = :productCode")
     Product findProductOne(@Param("productCode") String productCode);
+	
+	//해당 대분류 코드를 가진 소분류 데이터가 있는지 확인
+	@Query("SELECT COUNT(p) > 0 FROM Product p WHERE p.product_b.productBCode = :productBCode")
+	boolean existsByProductBCode(@Param("productBCode") String productBCode);
 
     /**
      * Dashboard 상품 재고 수량 목록 출력을 위한 메서드
