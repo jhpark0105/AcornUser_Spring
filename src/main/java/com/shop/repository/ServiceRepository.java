7e6555fc0ff7a91a3f81d5cd659a58392da0d9f3
jehui park <regulus0105@gmail.com>
2025-01-01 18:41:00 +0900

    250101 예약하기 생성
    NoticeController 알람 주석, ReservationController,Process 생성, ServiceRepository 추가

--- src/main/java/com/shop/repository/ServiceRepository.java
+++ src/main/java/com/shop/repository/ServiceRepository.java
@@ -6,10 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shop.dto.ServiceDto;
 import com.shop.entity.Service;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface ServiceRepository extends JpaRepository<Service, String>{
 	List<ServiceDto> findByServiceCode(String serviceCode);
 
 	boolean existsById(String serviceCode);
+
+//	@Query("select serviceCode, serviceName, serviceCnt, servicePrice from Service")
+//	List<Service> findServiceDetails();
+@Query("select new com.shop.dto.ServiceDto(s.serviceCode, s.serviceName, s.servicePrice) from Service s")
+List<ServiceDto> findServiceDetails();
 }
